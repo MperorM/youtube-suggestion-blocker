ef623e7979e8b215825dd969d0dfc68bfe3f711b
Mathias-bonde <mathias.bonde@ibm.com>
2019-07-14 21:36:31 +0200

    first release

--- youtubeblock.js
+++ youtubeblock.js
@@ -1,5 +1,3 @@
-// sleep(2000);
-
 var suggestions = document.getElementsByClassName('style-scope ytd-watch-next-secondary-results-renderer');
 var startpage = document.getElementsByClassName('style-scope ytd-page-manager');
 var startpage2 = document.getElementsByClassName('style-scope ytd-browse');
@@ -19,26 +17,8 @@ if (recommended.textContent === 'Recommended' || recommended.textContent === 'To
     startpage3[i].style.display = "none";
   }
   startpage3.remove();
-  // var cgtn_link = document.createElement('cgtn');
-  // var link_text = document.createTextNode('CLICK TO WATCH CGTN LIVE');
-  // cgtn_link.appendChild(link_text);
-  // cgtn_link.title = "CGTN LIVESTREAM LINK";
-  // cgtn_link.href = 'https://www.youtube.com/watch?v=ofUtSOORUmk';
-  // cgtn_link.style.display = 'block';
-  // document.body.appendChild(cgtn_link);
-  //startpage[0].parentNode.insertBefore(cgtn_link, startpage[0].nextSibling);
 }
 
 for (var i = 0; i < suggestions.length; i++) {
   suggestions[i].style.display = "none";
 }
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// waiting_time = sleep(2000);
-// waiting_time.then(function(){
-//     console.log(url.match(subreddit));
-//     console.log('HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHhh')
-// });
